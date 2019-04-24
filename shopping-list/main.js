cb7e90ec2c4eaf587e46c20d8763c49993c8f466
pakhomova <evgpakhomova@gmail.com>
2019-04-24 13:13:10 +0300

    create add window

--- shopping-list/main.js
+++ shopping-list/main.js
@@ -30,8 +30,8 @@ app.on('ready', function() {
 function createAddWindow() {
   //create new window
   addWindow = new BrowserWindow({
-    width: 200,
-    height: 300,
+    width: 300,
+    height: 200,
     title: 'Add Shopping List Item'
   });
   //load html into window

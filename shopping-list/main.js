305099bf6f918ae11a26aca4a66891aa7e33b233
pakhomova <evgpakhomova@gmail.com>
2019-04-26 09:36:34 +0300

    catch new item from form in addWindow

--- shopping-list/main.js
+++ shopping-list/main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addWindow;
@@ -52,6 +52,13 @@ function createAddWindow() {
     addWindow = null;
   });
 }
+
+//catch item:add
+ipcMain.on('item:add', function(event, item) {
+  console.log(item);
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close();
+});
 
 //create menu template
 const mainMenuTemplate = [
